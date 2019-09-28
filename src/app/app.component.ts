e470bc700bbb1f87d74bae1e568aeb24c42e5c60
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-09-29 00:04:52 +0300

    Add screen rotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,20 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private screenOrientation: ScreenOrientation
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    if (this.platform.is('cordova')) {
-      console.log('Cordova is available');
-    } else {
-      console.log('Cordova is not available');
-    }
-
     this.platform.ready().then(() => {
+      if (this.platform.is('cordova')) {
+        console.log('Cordova is available');
+        this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+      } else {
+        console.log('Cordova is not available');
+      }
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
